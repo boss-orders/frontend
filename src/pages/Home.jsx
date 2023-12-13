b9cab4402622b76de0d60dc8777970a5a5485c14
eiki0920 <s2122039@stu.musashino-u.ac.jp>
2023-12-13 22:54:21 +0900

    testAPI呼び出し

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,14 +1,23 @@
 import React, { useEffect } from "react";
 function Home() {
   useEffect(() => {
-    fetch("http://localhost:3001/api/v1/test")
+    console.log("aa");
+    fetch("http://localhost:8080/api/posts/", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        content: "test",
+      }),
+    })
       .then((res) => {
         return res.json();
       })
       .then((ans) => {
         console.log(ans);
       });
-  });
+  }, []);
   return <div className="home">hello</div>;
 }
 
